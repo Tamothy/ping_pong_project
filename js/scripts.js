b70ca59b614dc2c1c7e58c34f18818a9dba1ef70
Tam Nguyen <mr.tamron@gmail.com>
2016-06-24 16:51:05 -0700

    fix indentation in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
 var pingPongNumber = function(num) {
   for (var index = 1; index <= num; index += 1) {
     if (index % 15 === 0) {
-         $(".results").append("<li>" + "pingpong" + "</li>");
+      $(".results").append("<li>" + "pingpong" + "</li>");
     } else if (index % 3 === 0) {
-         $(".results").append("<li>" + "ping" + "</li>");
+      $(".results").append("<li>" + "ping" + "</li>");
     } else if (index % 5 === 0) {
-         $(".results").append("<li>" + "pong" + "</li>");
+      $(".results").append("<li>" + "pong" + "</li>");
     } else {
-         $(".results").append("<li>" + index + "</li>");
+      $(".results").append("<li>" + index + "</li>");
     }
   }
 };
